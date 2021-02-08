ea96c61143fb6d03d7e550a072d5ea86a378728c
Chris Marsh <chrismarsh.c2@gmail.com>
2021-02-08 11:49:34 -0600

    bump version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_installed_gdal_version():
 
 setup(
     name='CHM',
-    version='1.0.3',
+    version='1.0.4',
     description='python tools for CHM',
     license='MIT',
     packages=find_packages(),
