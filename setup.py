d4e48eddce9a9dd2e0466e72ed8e389f4a25069a
Chris Marsh <chrismarsh.c2@gmail.com>
2022-03-24 17:07:45 -0600

    fix setup.py regression

--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@ from setuptools import setup
 import subprocess
 from setuptools import find_packages
 import glob
+import packaging.version
 
 def get_installed_gdal_version():
     try:
-        version = subprocess.run(["gdal-config","--version"], stdout=subprocess.PIPE).stdout.decode()
+        version = subprocess.run(["gdal-config", "--version"], stdout=subprocess.PIPE).stdout.decode()
 
         version = version.replace('\n', '')
-        version = "=="+version+".*"
+        # pygdal doesn't always have the most up todate version so we will need to swtich to pygdal-chm if it is not available
+        # 3.3.2 is most recent as of this version
+        chm = ''
+        if packaging.version.parse(version) > packaging.version.parse("3.4.1"):
+            chm = '-chm'
+
+        version = chm + "==" + version + ".*"
         return version
+
     except FileNotFoundError as e:
         raise(""" ERROR: Could not find the system install of GDAL. 
                   Please install it via your package manage of choice.
@@ -18,14 +26,14 @@ def get_installed_gdal_version():
 
 setup(
     name='CHM',
-    version='1.0.14',
+    version='1.0.20',
     description='python tools for CHM',
     license='MIT',
     packages=find_packages(),
     author='Chris Marsh',
     author_email='chris.marsh@usask.ca',
     install_requires=['vtk','numpy','xarray>=0.18.1','netCDF4','pandas',
-                      'pygdal-chm'+get_installed_gdal_version(), 'dask[complete]',
+                      'pygdal'+get_installed_gdal_version(), 'dask[complete]',
                       'pyvista>0.29', 'pyESMF~=8.2.1b3','rioxarray','rasterio', 'ninja'],
     scripts=glob.glob("scripts/*.py"),
 )
