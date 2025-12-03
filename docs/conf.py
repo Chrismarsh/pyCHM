18109fc9ba121e1916cc5bfc80c208f5d8d2ac96
Chris Marsh <chrismarsh.c2@gmail.com>
2025-12-03 17:17:20 +0000

    fix darkmode notebook

--- docs/conf.py
+++ docs/conf.py
@@ -131,6 +131,9 @@ master_doc = 'index'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    "fix-notebook-darkmode.css",
+]
 
 # Extensions to theme docs
 def setup(app):
